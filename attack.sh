346c2d0cdb51f0d14f882e804ee15f29a1ddaba5
AbdelhafidRaoufSoge <raouf.abdelhafid@outlook.fr>
2024-07-14 14:58:44 +0100

    update

--- attack.sh
+++ attack.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 vegeta attack -rate=50 -duration=30s -targets=targets.txt > results.bin
-vegeta report results.bin# Generate the HTML plot
+
+vegeta report results.bin
+
 vegeta plot results.bin > plot.html
